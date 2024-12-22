ebb6aa60d8efcd34ff45a12c15f46bf37047c875
Miunn <remi@remicaulier.fr>
2024-12-22 17:10:57 +0100

    Shar link layout and fix windo server side error

--- src/components/folders/ShareFolderDialog.tsx
+++ src/components/folders/ShareFolderDialog.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     Dialog, DialogClose,
     DialogContent,
@@ -6,26 +8,28 @@ import {
     DialogHeader,
     DialogTitle, DialogTrigger
 } from "@/components/ui/dialog";
-import {Button} from "@/components/ui/button";
-import {useTranslations} from "next-intl";
-import {Share, X} from "lucide-react";
-import {Input} from "@/components/ui/input";
-import {toast} from "@/hooks/use-toast";
-import {Separator} from "@/components/ui/separator";
-import {Label} from "@/components/ui/label";
-import React, {useEffect, useRef, useState} from "react";
-import {ScrollArea} from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
+import { Share, X } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/hooks/use-toast";
+import { Separator } from "@/components/ui/separator";
+import { Label } from "@/components/ui/label";
+import React, { useEffect, useRef, useState } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { AccessToken } from "@prisma/client";
+import { z } from "zod";
 
-export const ShareFolderDialog = ({folder}: { folder: any }) => {
+export const ShareFolderDialog = ({ folder }: { folder: any }) => {
 
     const [emailList, setEmailList] = useState<string[]>([]);
     const [email, setEmail] = useState<string>("");
     const emailScroll = useRef<HTMLDivElement>(null);
     const t = useTranslations("folders.dialog.share");
 
-    const readShareLink = `${window.location.origin}/dashboard/folders/${folder.id}?share=${folder.AccessToken.filter((token: AccessToken) => token.permission === "READ")[0].token}`;
-    const writeShareLink = `${window.location.origin}/dashboard/folders/${folder.id}?share=${folder.AccessToken.filter((token: AccessToken) => token.permission === "WRITE")[0].token}`;
+    const [readShareLink, setReadShareLink] = useState("");
+    const [writeShareLink, setWriteShareLink] = useState("");
+
     const copyToClipboard = (link: string) => {
         navigator.clipboard.writeText(link).then(() => {
             toast({
@@ -42,7 +46,13 @@ export const ShareFolderDialog = ({folder}: { folder: any }) => {
     }
 
     const addEmail = (email: string) => {
-        if (!email || email.length === 0 || emailList.includes(email)) {
+        const emailSchema = z.string().email();
+        if (!email || email.length === 0 || emailList.includes(email) || !emailSchema.safeParse(email).success) {
+            toast({
+                title: t('fields.email.error.title'),
+                description: t('fields.email.error.description'),
+                variant: "destructive"
+            })
             return;
         }
         setEmailList([...emailList, email]);
@@ -53,57 +63,65 @@ export const ShareFolderDialog = ({folder}: { folder: any }) => {
         if (!emailScroll.current) {
             return;
         }
+        
         emailScroll.current!.scrollIntoView(false);
     }, [emailList]);
 
+    useEffect(() => {
+        setReadShareLink(`${window.location.origin}/dashboard/folders/${folder.id}?share=${folder.AccessToken.filter((token: AccessToken) => token.permission === "READ")[0].token}`);
+        setWriteShareLink(`${window.location.origin}/dashboard/folders/${folder.id}?share=${folder.AccessToken.filter((token: AccessToken) => token.permission === "WRITE")[0].token}`);
+    });
+
     return (
         <Dialog>
-            <DialogTrigger>
+            <DialogTrigger asChild>
                 <Button variant="outline">
-                    <Share/> {t('trigger')}
+                    <Share /> {t('trigger')}
                 </Button>
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent className="max-w-xl">
                 <DialogHeader>
                     <DialogTitle>{t('title')}</DialogTitle>
                     <DialogDescription>{t('description')}</DialogDescription>
                 </DialogHeader>
 
                 <Label>{t('fields.link.label')}</Label>
-                <div className={"flex gap-3 w-full"}>
+                <div className={"grid gap-3 w-full items-center"} style={{
+                    gridTemplateColumns: "0.5fr 1fr auto"
+                }}>
+                    <p className="text-sm text-nowrap">Read-only link</p>
                     <Input placeholder={t('fields.link.placeholder')} disabled={true}
-                           value={readShareLink}/>
+                        value={readShareLink} />
                     <Button onClick={() => copyToClipboard(readShareLink)}>
                         {t('button.copy')}
                     </Button>
-                </div>
-                <div className={"flex gap-3 w-full"}>
+                    <p className="text-sm text-nowrap">Read and write link</p>
                     <Input placeholder={t('fields.link.placeholder')} disabled={true}
-                           value={writeShareLink}/>
+                        value={writeShareLink} />
                     <Button onClick={() => copyToClipboard(writeShareLink)}>
                         {t('button.copy')}
                     </Button>
                 </div>
-                <Separator orientation={"horizontal"} className={"my-4"}/>
+                <Separator orientation={"horizontal"} className={"my-4"} />
 
                 <Label htmlFor={"share-email"}>{t('fields.email.label')}</Label>
                 <div className={"flex gap-3 w-full"}>
                     <Input id={"share-email"} placeholder={t('fields.email.placeholder')} value={email}
-                           onChange={(v) => setEmail(v.currentTarget.value)}/>
+                        onChange={(v) => setEmail(v.currentTarget.value)} />
                     <Button onClick={() => addEmail(email)}>
                         {t('button.emailAdd')}
                     </Button>
                 </div>
                 <ScrollArea className={"max-h-40"}>
                     <div ref={emailScroll}>
-                    {emailList.map((email: string) => (
-                        <div key={email} className={"flex gap-1 w-full my-1"}>
-                            <Button onClick={() => setEmailList(emailList.filter((e) => e !== email))} variant="ghost" size="icon">
-                                <X className={"w-4 h-4 text-red-500"} />
-                            </Button>
-                            <Input value={email} disabled={true}/>
-                        </div>
-                    ))}
+                        {emailList.map((email: string) => (
+                            <div key={email} className={"flex gap-1 w-full my-1"}>
+                                <Button onClick={() => setEmailList(emailList.filter((e) => e !== email))} variant="ghost" size="icon">
+                                    <X className={"w-4 h-4 text-red-500"} />
+                                </Button>
+                                <Input value={email} disabled={true} />
+                            </div>
+                        ))}
                     </div>
                 </ScrollArea>
                 <DialogFooter>
