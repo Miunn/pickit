291503438c73f8295ee123a327d67047c8269491
Miunn <remi@remicaulier.fr>
2025-01-03 19:07:36 +0100

    Catch line

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,3 @@
-import "../globals.css";
 import Header from "@/components/layout/Header";
 
 // This page only renders when the app is built statically (output: 'export')
@@ -7,6 +6,11 @@ export default function RootPage({ params }: { params: { locale: string } }) {
     return (
         <main>
             <Header locale={params.locale} />
+
+            <h1 className="text-6xl text-center mt-36">Save <span>moments</span> of life</h1>
+            <p className="text-center mt-16 text-lg max-w-md mx-auto">
+                Save your memories and share them with your loved ones with our secure and easy-to-use platform.
+            </p>
         </main>
     )
 }
