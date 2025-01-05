2637cb8260b4291438b2486082d89b9cb0f31f06
Miunn <remi@remicaulier.fr>
2025-01-05 21:36:26 +0100

    Fix y-padding on unverified email text

--- src/components/layout/UnverifiedEmail.tsx
+++ src/components/layout/UnverifiedEmail.tsx
@@ -17,7 +17,7 @@ export default function UnverifiedEmail({ locale, userDeletionDate }: { locale: 
 
     return (
         <Link ref={ref} href={`/${locale}/dashboard/account`} className="relative w-full h-12 bg-orange-400 hover:bg-orange-500 flex justify-center items-center cursor-pointer mb-4">
-            <p className="text-center text-sm font-bold p-9">{t('message', {date: intlFormatter.dateTime(userDeletionDate, { day: "numeric", year: "numeric", month: "long" })})}</p>
+            <p className="text-center text-sm font-bold px-9">{t('message', {date: intlFormatter.dateTime(userDeletionDate, { day: "numeric", year: "numeric", month: "long" })})}</p>
             <button onClick={remove} className="unset absolute w-4 h-4 right-4 top-1/2 -translate-y-1/2 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring disabled:pointer-events-none">
                 <Cross2Icon />
             </button>
