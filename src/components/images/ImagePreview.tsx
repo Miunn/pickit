5ed583672e00d6543ea4641db04ce76785fd45cc
Miunn <remi@remicaulier.fr>
2025-01-07 16:13:07 +0100

    Select padding

--- src/components/images/ImagePreview.tsx
+++ src/components/images/ImagePreview.tsx
@@ -38,7 +38,7 @@ export const ImagePreview = ({ image, selecting, setSelecting, selected, setSele
                         onClick(image.id);
                     }
                 }} style={{ all: "unset", cursor: "pointer" }}>
-                    <div className={`inline-block w-64 rounded-2xl ${selected.includes(image.id) ? "bg-blue-100" : ""}`}>
+                    <div className={`inline-block w-64 p-2 rounded-2xl ${selected.includes(image.id) ? "bg-blue-100" : ""}`}>
                         <div className={`relative h-36 mb-4 flex justify-center items-center`}>
                             <Image src={`/api/folders/${image.folderId}/images/${image.id}?share=${shareToken}&h=${shareHashPin}`} alt={image.name}
                                 className={"relative border border-black rounded-xl object-cover"} sizes="33vw" fill />
