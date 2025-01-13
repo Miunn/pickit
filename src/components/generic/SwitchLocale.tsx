b5927ee996197374806aa47d073f88d45388d50a
Miunn <remi@remicaulier.fr>
2025-01-13 14:31:20 +0100

    Unused imports

--- src/components/generic/SwitchLocale.tsx
+++ src/components/generic/SwitchLocale.tsx
@@ -3,12 +3,9 @@
 import { cn, switchLocaleUrl } from "@/lib/utils";
 import { Languages } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 
 export default function SwitchLocale({ locale, className }: { locale: string, className?: string }) {
-    const pathname = usePathname();
-
     return (
         <Button variant={"ghost"} size={"icon"} asChild>
         <Link href={switchLocaleUrl(window.location.pathname + window.location.search, locale === "en" ? "fr" : "en")} className={cn("font-normal", className)}>
