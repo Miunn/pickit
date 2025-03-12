1a7e3c9f876fb4b0d70ce71606d2a93c15c74ae2
Miunn <remi@remicaulier.fr>
2025-03-12 16:49:45 +0100

    Start of a footer

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import FeatureCarouselPreview from "@/components/FeatureCarouselPreview";
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
@@ -7,71 +8,62 @@ import { ArrowRight } from "lucide-react";
 export default function RootPage({ params }: { params: { locale: string } }) {
 
     return (
-        <main>
-            <Header className="sticky top-0 z-50 bg-background/90 backdrop-blur mb-11" locale={params.locale} />
+        <>
+            <main>
+                <Header className="sticky top-0 z-50 bg-background/90 backdrop-blur mb-11" locale={params.locale} />
 
-            <div className="relative bg-black h-[600px] rounded-3xl mx-6 text-white flex flex-col justify-center gap-11">
-                <div className="relative w-full max-w-7xl mx-auto">
-                    <h1 className="text-6xl mb-8">Save moments of life</h1>
+                <div className="relative bg-primary h-[600px] rounded-3xl mx-6 text-white flex flex-col justify-center gap-11">
+                    <div className="relative w-full max-w-7xl mx-auto">
+                        <h1 className="text-6xl mb-8">Save moments of life</h1>
 
-                    <p className="text-xl mb-8">
-                        Save your memories and share them to your loved ones<br />
-                        with our secure and easy-to-use platform.
-                    </p>
+                        <p className="text-xl mb-8">
+                            Save your memories and share them to your loved ones<br />
+                            with our secure and easy-to-use platform.
+                        </p>
 
-                    <div className="flex items-center gap-4">
-                        <Button className="p-7 rounded-full">
-                            Get started <ArrowRight />
-                        </Button>
-                        <Button variant={"link"} className="text-white">
-                            See more
-                        </Button>
-                    </div>
-
-                    <div className="flex items-center gap-16 mt-16">
-                        <div>
-                            <Label className="text-4xl font-bold tracking-wider">1000</Label>
-                            <p>Exemple stat</p>
-                        </div>
-
-                        <div>
-                            <Label className="text-4xl font-bold tracking-wider">1000</Label>
-                            <p>Exemple stat</p>
+                        <div className="flex items-center gap-4">
+                            <Button className="p-7 rounded-full" variant={"secondary"}>
+                                Get started <ArrowRight />
+                            </Button>
+                            <Button variant={"link"} className="text-white">
+                                See more
+                            </Button>
                         </div>
                     </div>
+                    <div className="absolute bottom-0 left-[296px] -translate-x-1/2 translate-y-1/2">
+                        <div className="absolute bottom-0 right-32 translate-y-1/2 w-32 h-32 bg-primary">
+                            <div className="w-32 h-32 bg-white rounded-tr-lg "></div>
+                        </div>
+                        <div className="w-32 h-32 bg-primary rounded-full"></div>
+                        <div className="absolute bottom-0 left-32 translate-y-1/2 w-32 h-32 bg-primary">
+                            <div className="w-32 h-32 bg-white rounded-tl-lg "></div>
+                        </div>
 
-                </div>
-                <div className="absolute bottom-0 left-[296px] -translate-x-1/2 translate-y-1/2">
-                    <div className="absolute bottom-0 right-32 translate-y-1/2 w-32 h-32 bg-black">
-                        <div className="w-32 h-32 bg-white rounded-tr-lg "></div>
+                        <div className="absolute left-1/2 -translate-x-1/2 w-24 h-24 rounded-full bg-primary mt-8"></div>
+
+                        <div className="absolute left-1/2 -translate-x-1/2 w-16 h-16 rounded-full bg-primary mt-40"></div>
+
+                        <div className="absolute left-1/2 -translate-x-1/2 w-8 h-8 rounded-full bg-primary mt-64"></div>
                     </div>
-                    <div className="w-32 h-32 bg-black rounded-full"></div>
-                    <div className="absolute bottom-0 left-32 translate-y-1/2 w-32 h-32 bg-black">
-                        <div className="w-32 h-32 bg-white rounded-tl-lg "></div>
+                </div>
+
+                <div className="max-w-7xl mx-auto grid grid-cols-3 my-32">
+                    <div className="col-start-2 col-span-2">
+                        <h3 className="text-5xl">Store & Share your photos easily</h3>
+
+                        <p>
+                            With Pickit, you can store and share your photos to your loved ones easily.
+                            Our platform is secure and easy to use, you can create albums and share them with your friends and family.
+                        </p>
                     </div>
-
-                    <div className="absolute left-1/2 -translate-x-1/2 w-24 h-24 rounded-full bg-black mt-8"></div>
-
-                    <div className="absolute left-1/2 -translate-x-1/2 w-16 h-16 rounded-full bg-black mt-40"></div>
-
-                    <div className="absolute left-1/2 -translate-x-1/2 w-8 h-8 rounded-full bg-black mt-64"></div>
                 </div>
-            </div>
 
-            <div className="max-w-7xl mx-auto grid grid-cols-3 my-32">
-                <div className="col-start-2 col-span-2">
-                    <h3 className="text-5xl">Store & Share your photos easily</h3>
+                <FeatureCarouselPreview />
 
-                    <p>
-                        With Pickit, you can store and share your photos to your loved ones easily.
-                        Our platform is secure and easy to use, you can create albums and share them with your friends and family.
-                    </p>
-                </div>
-            </div>
+                <div className="mt-64"></div>
 
-            <FeatureCarouselPreview />
-
-            <div className="mt-64"></div>
-        </main>
+            </main>
+            <Footer />
+        </>
     )
 }
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,51 @@
+import { useTranslations } from "next-intl";
+import { Label } from "../ui/label";
+import Link from "next/link";
+
+export default function Footer() {
+
+    const t = useTranslations("components.footer");
+
+    return (
+        <footer className="border-t">
+            <div className="max-w-7xl mx-auto py-12">
+                <div className="flex justify-between">
+                    <h1 className="text-7xl tracking-wider">Pickit</h1>
+
+                    <div className="flex gap-16">
+                        <div className="flex flex-col gap-4">
+                            <Label>{t('about.title')}</Label>
+
+                            <div className="flex flex-col gap-2">
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("about.features")}</Link>
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("about.pricing")}</Link>
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("about.contact")}</Link>
+
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-4">
+                            <Label>{t('documentation.title')}</Label>
+
+                            <div className="flex flex-col gap-2">
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("documentation.faq")}</Link>
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("documentation.support")}</Link>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-4">
+                            <Label>{t('legal.title')}</Label>
+
+                            <div className="flex flex-col gap-2">
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("legal.terms")}</Link>
+                                <Link href="#" className="text-sm font-medium text-gray-600">{t("legal.privacy")}</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <p className="w-fit ml-auto text-sm text-gray-600 mt-11">{t('copyright.text')}</p>
+            </div>
+        </footer>
+    )
+}
