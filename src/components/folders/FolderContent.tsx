1261bf27e2dfe5d00d87236942757594eda4d1d0
Miunn <remi@remicaulier.fr>
2025-03-26 12:09:58 +0100

    Folder content actions responsive

--- src/components/folders/FolderContent.tsx
+++ src/components/folders/FolderContent.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
-import { Download } from "lucide-react";
+import { ArrowDown, ArrowUp, Download, LayoutGrid, List, MoreHorizontal } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { UploadImagesDialog } from "@/components/images/UploadImagesDialog";
 import { ImagesGrid } from "@/components/images/ImagesGrid";
@@ -9,9 +9,11 @@ import { ShareFolderDialog } from "@/components/folders/ShareFolderDialog";
 import { FolderWithAccessToken, FolderWithCreatedBy, FolderWithImagesWithFolderAndComments } from "@/lib/definitions";
 import SortImages, { ImagesSortMethod } from "./SortImages";
 import { useQueryState } from 'nuqs'
-import { downloadClientFolder, getSortedFolderContent } from "@/lib/utils";
+import { downloadClientFolder } from "@/lib/utils";
 import ViewSelector, { ViewState } from "./ViewSelector";
 import ImagesList from "../images/ImagesList";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { useState } from "react";
 
 export interface FolderContentProps {
     folder: FolderWithCreatedBy & FolderWithImagesWithFolderAndComments & FolderWithAccessToken;
@@ -56,6 +58,9 @@ export const FolderContent = ({ folder, defaultView, isGuest }: FolderContentPro
         }
     });
 
+    const [openUpload, setOpenUpload] = useState(false);
+    const [openShare, setOpenShare] = useState(false);
+
     const downloadT = useTranslations("folders.download");
 
     return (
@@ -67,7 +72,7 @@ export const FolderContent = ({ folder, defaultView, isGuest }: FolderContentPro
                         : null
                 }</p>
 
-                <div className={"flex gap-4"}>
+                <div className={"hidden lg:flex gap-4"}>
                     <ViewSelector viewState={viewState} setViewState={setViewState} />
                     {viewState === ViewState.Grid
                         ? <SortImages sortState={sortState} setSortState={setSortState} />
@@ -81,6 +86,103 @@ export const FolderContent = ({ folder, defaultView, isGuest }: FolderContentPro
                         <Download className={"mr-2"} /> {t('actions.download')}
                     </Button>
                 </div>
+                <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger className="lg:hidden">
+                        <MoreHorizontal className="w-4 h-4" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuSub>
+                            <DropdownMenuSubTrigger>{t('views.label')}</DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem onClick={() => setViewState(ViewState.Grid)} className="flex items-center gap-3">
+                                        <LayoutGrid className="w-4 h-4" /> {t('views.options.grid')}
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => setViewState(ViewState.List)} className="flex items-center gap-3">
+                                        <List className="w-4 h-4" /> {t('views.options.list')}
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                        {viewState === ViewState.Grid
+                            ? <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>{t('sort.label')}</DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem onClick={(event) => {
+                                        event.preventDefault();
+                                        if (sortState === ImagesSortMethod.NameDesc) {
+                                            setSortState(ImagesSortMethod.NameAsc);
+                                        } else {
+                                            setSortState(ImagesSortMethod.NameDesc)
+                                        }
+                                    }} className="flex justify-between items-center gap-3">
+                                        {t('sort.options.name')}
+                                        {sortState === ImagesSortMethod.NameAsc
+                                            ? <ArrowUp className="w-4 h-4" />
+                                            : null
+                                        }
+                                        {sortState === ImagesSortMethod.NameDesc
+                                            ? <ArrowDown className="w-4 h-4" />
+                                            : null
+                                        }
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={(event) => {
+                                        event.preventDefault();
+                                        if (sortState === ImagesSortMethod.SizeDesc) {
+                                            setSortState(ImagesSortMethod.SizeAsc);
+                                        } else {
+                                            setSortState(ImagesSortMethod.SizeDesc)
+                                        }
+                                    }} className="flex justify-between items-center gap-3">
+                                        {t('sort.options.size')}
+                                        {sortState === ImagesSortMethod.SizeAsc
+                                            ? <ArrowUp className="w-4 h-4" />
+                                            : null
+                                        }
+                                        {sortState === ImagesSortMethod.SizeDesc
+                                            ? <ArrowDown className="w-4 h-4" />
+                                            : null
+                                        }
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={(event) => {
+                                        event.preventDefault();
+                                        if (sortState === ImagesSortMethod.DateDesc) {
+                                            setSortState(ImagesSortMethod.DateAsc);
+                                        } else {
+                                            setSortState(ImagesSortMethod.DateDesc)
+                                        }
+                                    }} className="flex justify-between items-center gap-3">
+                                        {t('sort.options.date')}
+                                        {sortState === ImagesSortMethod.DateAsc
+                                            ? <ArrowUp className="w-4 h-4" />
+                                            : null
+                                        }
+                                        {sortState === ImagesSortMethod.DateDesc
+                                            ? <ArrowDown className="w-4 h-4" />
+                                            : null
+                                        }
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                            : null
+                        }
+                        {!!!isGuest
+                            ? <DropdownMenuItem onClick={() => setOpenUpload(true)}>
+                                {t('upload.label')}
+                            </DropdownMenuItem>
+                            : null}
+                        {!!!isGuest
+                            ? <DropdownMenuItem onClick={() => setOpenShare(true)}>
+                                {t('share.label')}
+                            </DropdownMenuItem>
+                            : null}
+                        <DropdownMenuItem onClick={() => downloadClientFolder(folder, downloadT)}>
+                            {t('download.label')}
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                <UploadImagesDialog open={openUpload} setOpen={setOpenUpload} folderId={folder.id} />
+                <ShareFolderDialog open={openShare} setOpen={setOpenShare} folder={folder} />
             </h3>
 
             <div className="flex-1 overflow-auto">
--- src/components/images/UploadImagesDialog.tsx
+++ src/components/images/UploadImagesDialog.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {useTranslations} from "next-intl";
+import { useTranslations } from "next-intl";
 import {
     Dialog,
     DialogContent,
@@ -9,66 +9,35 @@ import {
     DialogTitle,
     DialogTrigger
 } from "@/components/ui/dialog";
-import {Button} from "@/components/ui/button";
-import {ImageUp} from "lucide-react";
-import {useState} from "react";
+import { Button } from "@/components/ui/button";
+import { ImageUp } from "lucide-react";
+import { useState } from "react";
 import UploadImagesForm from "./UploadImagesForm";
 
-export const UploadImagesDialog = ({folderId}: { folderId: string }) => {
+export const UploadImagesDialog = ({ folderId, open, setOpen }: { folderId: string, open?: boolean, setOpen?: React.Dispatch<React.SetStateAction<boolean>> }) => {
     const t = useTranslations("images.dialog.upload");
-    const [open, setOpen] = useState(false);
+
+    const [internalOpen, setInternalOpen] = useState(false);
+
+    const openState = open ?? internalOpen;
+    const setOpenState = setOpen ?? setInternalOpen;
 
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
-                <Button variant="outline" className={"w-fit flex items-center"}>
-                    <ImageUp className={"mr-2"}/> {t('trigger')}
-                </Button>
-            </DialogTrigger>
+        <Dialog open={openState} onOpenChange={setOpenState}>
+            {!open && !setOpen
+                ? <DialogTrigger asChild>
+                    <Button variant="outline" className={"w-fit flex items-center"}>
+                        <ImageUp className={"mr-2"} /> {t('trigger')}
+                    </Button>
+                </DialogTrigger>
+                : null
+            }
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>{t('title')}</DialogTitle>
                     <DialogDescription>{t('description')}</DialogDescription>
                 </DialogHeader>
-                <UploadImagesForm folderId={folderId} onUpload={() => setOpen(false)} />
-                {/* <Form {...form}>
-                    <form onSubmit={form.handleSubmit(submitImages)} className="space-y-8">
-                        <FormField
-                            control={form.control}
-                            name="images"
-                            render={({field: {value, onChange, ...fieldProps}}) => (
-                                <FormItem>
-                                    <FormLabel>{t('fields.images.label')}</FormLabel>
-                                    <FormControl>
-                                        <FileUploader
-                                            multiple={true}
-                                            maxSize={1024 * 1024 * 5}
-                                            maxFileCount={999}
-                                            accept={{
-                                                'image/png': ['.png'],
-                                                'image/jpeg': ['.jpg', '.jpeg'],
-                                                'image/gif': ['.gif'],
-                                                'image/webp': ['.webp'],
-                                            }}
-                                            onValueChange={(files) => onChange(files)}
-                                            {...fieldProps}
-                                        />
-                                    </FormControl>
-                                    <FormDescription>{t('fields.images.description')}</FormDescription>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-
-                        <DialogFooter>
-                            {loading
-                                ? <Button disabled={true}><Loader2 className={"w-4 h-4 mr-2 animate-spin"}/> {t('submitting')}
-                                </Button>
-                                : <Button type={"submit"}>{t('submit')}</Button>
-                            }
-                        </DialogFooter>
-                    </form>
-                </Form> */}
+                <UploadImagesForm folderId={folderId} onUpload={() => setOpenState(false)} />
             </DialogContent>
         </Dialog>
     )
