fa0ccbee0d0e13d00523539ed96d84f67ee67c3d
Miunn <remi@remicaulier.fr>
2025-04-09 22:40:59 -0400

    Try to fix failing gh action with webpack alias resolution

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['@prisma/client', 'pg', 'bcrypt-edge'],
+    },
+    webpack: (config) => {
+        config.resolve.alias.canvas = false;
+        config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+        return config;
     }
 };
 
