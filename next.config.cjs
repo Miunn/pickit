cf2ffe92c526cb4cbb3f37217c9bb258df6c149b
Rémi Caulier <remi@remicaulier.fr>
2025-04-11 20:52:39 -0400

    Staging (#40)
    Merge #40

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,14 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import path from 'path';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack(config) {
+        config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+        return config;
+    },
     experimental: {
         serverComponentsExternalPackages: ['@prisma/client', 'pg', 'bcrypt-edge'],
     }
