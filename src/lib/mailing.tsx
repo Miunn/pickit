4397761610af61666fa157397bf728a97b6c89dc
Miunn <remi@remicaulier.fr>
2025-04-23 15:05:49 -0400

    Extract mail secure to env

--- src/lib/mailing.tsx
+++ src/lib/mailing.tsx
@@ -3,7 +3,7 @@ import * as nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: parseInt(process.env.MAIL_PORT || "465"),
-  secure: true, // true for port 465, false for other ports
+  secure: process.env.MAIL_SECURE === "true", // true for port 465, false for other ports
   auth: {
     user: process.env.MAIL_SENDER,
     pass: process.env.MAIL_PASSWORD,
