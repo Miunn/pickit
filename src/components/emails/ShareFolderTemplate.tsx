0e17024950612a868fb9eb553d3b532e47e20097
Miunn <remi@remicaulier.fr>
2025-04-28 23:49:42 -0400

    Email header logo

--- src/components/emails/ShareFolderTemplate.tsx
+++ src/components/emails/ShareFolderTemplate.tsx
@@ -33,7 +33,7 @@ export default function ShareFolderTemplate({ name, folderName, link, isLocked, 
                                 <Section width="100%">
                                     <tr>
                                         <td align="center" valign="middle">
-                                            <Img src={`${baseUrl}/static/favicon.jpg`} alt={process.env.NEXT_PUBLIC_APP_NAME} className="inline-block max-w-[200px] max-h-[40px]" />
+                                            <Img src={`${baseUrl}/static/logo-primary-svg.svg`} alt={process.env.NEXT_PUBLIC_APP_NAME} className="inline-block max-w-[200px] max-h-[40px]" />
                                             <Text className="inline-block text-[1.5rem] m-0 pl-[10px] font-semibold">{process.env.NEXT_PUBLIC_APP_NAME}</Text>
                                         </td>
                                     </tr>
