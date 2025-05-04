c5689f98de97c394ae603d9e07155344c51f6917
Miunn <remi@remicaulier.fr>
2025-05-03 22:02:52 -0400

    Unused imports

--- src/components/layout/HeaderBreadcumb.tsx
+++ src/components/layout/HeaderBreadcumb.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "../ui/breadcrumb";
 import { UserAdministration } from "@/lib/definitions";
