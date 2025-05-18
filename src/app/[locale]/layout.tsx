fd9fb9dbfe9ee960209bc321340b5b7eca98ff6d
Miunn <remi@remicaulier.fr>
2025-05-18 17:19:03 -0400

    Theme color for discord

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "Echomori",
     description: "Upload and share images with ease.",
+    themeColor: "#1f7551",
 };
 
 type Props = {
