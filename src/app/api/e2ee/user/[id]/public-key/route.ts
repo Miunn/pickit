09ea15dc63bf72aa977820372d7a9c7b3b6c4570
Miunn <remi@remicaulier.fr>
2025-06-17 21:01:12 -0400

    fix(api): params promise

--- src/app/api/e2ee/user/[id]/public-key/route.ts
+++ src/app/api/e2ee/user/[id]/public-key/route.ts
@@ -1,13 +1,15 @@
 import { prisma } from "@/lib/prisma";
 import { getCurrentSession } from "@/lib/session";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, props: { params: Promise<{ id: string }> }) {
     const session = await getCurrentSession();
     if (!session?.user) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    
+
+    const params = await props.params;
+
     const user = await prisma.user.findUnique({
         where: { id: params.id },
         select: { publicKey: true }
