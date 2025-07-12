e5128a6dd0acab1ec74cbddacd6cf1cbe86fdc0d
Miunn <remi@remicaulier.fr>
2025-07-11 23:57:42 -0400

    feat(subscription): preview plan

--- src/components/billing/ChangePlanDialog.tsx
+++ src/components/billing/ChangePlanDialog.tsx
@@ -1,24 +1,43 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
 import { useTranslations } from "next-intl";
 import { usePricingContext } from "@/context/PricingContext";
 import { useSession } from "@/providers/SessionProvider";
 import { Plan } from "@prisma/client";
 import { useFormatter } from "next-intl";
 import { updateSubscription } from "@/actions/subscriptions";
+import { Circle, Gift, Loader2 } from "lucide-react";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import { useState } from "react";
 
 export default function ChangePlanDialog({ open, setOpen, newPlan, nextAmount, isYearly }: { open: boolean, setOpen: (open: boolean) => void, newPlan: Plan, nextAmount: number, isYearly: boolean }) {
     const t = useTranslations("billing.changePlanPreview");
     const formatter = useFormatter();
     const { user } = useSession();
     const { plans, getPlanPrice, getPriceId } = usePricingContext();
-    
+    const credits = nextAmount < 0 ? Math.abs(nextAmount) : 0;
+
+    const [isLoading, setIsLoading] = useState(false);
+
     if (!user) {
         return null;
     }
 
     const handleChangePlan = async () => {
-        setOpen(false);
+        setIsLoading(true);
         await updateSubscription(getPriceId(newPlan, isYearly ? "yearly" : "monthly"));
+        setIsLoading(false);
+        setOpen(false);
+    }
+
+    const getCreditsOrAmountText = () => {
+        if (nextAmount < 0) {
+            return (
+                <span className="flex items-center gap-1">0€ <span className="text-[#9b59b6] flex items-center">(<Gift className="size-4" /> {t("freeCredits", { credits: Math.abs(nextAmount) })})</span></span>
+            )
+        }
+
+        return <span>{nextAmount} €</span>
     }
 
     return (
@@ -28,20 +47,29 @@ export default function ChangePlanDialog({ open, setOpen, newPlan, nextAmount, i
                     <DialogTitle>{t("title")}</DialogTitle>
                 </DialogHeader>
 
-                <div className="flex flex-col gap-2">
-                    <div>
-                        <p>{t("before.title")}</p>
-                        <p>{plans[user.plan].name}</p>
+                <div className="h-[250px] w-full flex gap-4">
+                    <div className="flex-1 flex flex-col justify-between items-end">
+                        <Label className="text-end">{t("before.title")}</Label>
+                        <Label className="text-end">{t("now.title")}</Label>
+                        <Label className="text-end">{formatter.dateTime(isYearly ? new Date().setFullYear(new Date().getFullYear() + 1) : new Date().setMonth(new Date().getMonth() + 1), { dateStyle: "long" })}</Label>
                     </div>
-                    <div>
-                        <p>{t("now.title")}</p>
-                        <p>{plans[newPlan].name} ({nextAmount})</p>
+                    <div className="flex flex-col justify-between items-center">
+                        <div className="size-4 rounded-full border-primary border-4" />
+                        <div className="flex-1 w-1 bg-gradient-to-b from-primary to-[#9b59b6]" />
+                        <div className="size-4 rounded-full border-[#9b59b6] border-4" />
+                        <div className="flex-1 w-1 bg-gradient-to-b from-[#9b59b6] to-[#f1c40f]" />
+                        <div className="size-4 rounded-full border-[#f1c40f] border-4" />
                     </div>
-                    <div>
-                        <p>{formatter.dateTime(isYearly ? new Date().setFullYear(new Date().getFullYear() + 1) : new Date().setMonth(new Date().getMonth() + 1), { dateStyle: "long" })}</p>
-                        <p>{plans[newPlan].name} {getPlanPrice(newPlan)}</p>
+                    <div className="flex-1 flex flex-col justify-between items-start">
+                        <Label>{plans[user.plan].name}</Label>
+                        <Label>{plans[newPlan].name} - {getCreditsOrAmountText()}</Label>
+                        <Label>{plans[newPlan].name} - {Math.max(0, getPlanPrice(newPlan) - credits)}€</Label>
                     </div>
                 </div>
+
+                <DialogFooter>
+                    <Button onClick={handleChangePlan} disabled={isLoading}>{isLoading ? <Loader2 className="size-4 animate-spin" /> : t("changePlan")}</Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
