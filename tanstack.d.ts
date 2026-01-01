d9160bfb8d2cda029a4d1313ef72fd4f9f0d4f24
Rémi Caulier <remi@remicaulier.fr>
2026-01-01 21:53:44 +0100

    fix(tanstack): optionnal locale metadata

--- tanstack.d.ts
+++ tanstack.d.ts
@@ -3,7 +3,7 @@ import { createFormatter, createTranslator } from "next-intl";
 
 declare module "@tanstack/table-core" {
     export interface TableMeta {
-        locale: string;
+        locale?: string;
         imagesListActions?: {
             setCarouselOpen: React.Dispatch<React.SetStateAction<boolean>>;
             setStartIndex: React.Dispatch<React.SetStateAction<number>>;
