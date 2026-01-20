947d0e1f03bf8ea16461b030e6fd6ea071abc793
Rémi Caulier <remi@remicaulier.fr>
2026-01-20 21:30:53 +0100

    refactor(links): combine tables to avoid duplication

--- src/components/links/links-data-table.tsx
+++ src/components/links/links-data-table.tsx
@@ -1,41 +1,48 @@
 "use client";
 
 import { DataTable } from "@/components/ui/data-table";
+import CreateAccessTokenDialog from "@/components/accessTokens/CreateAccessTokenDialog";
 import { DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Trash2 } from "lucide-react";
+import { Link, Trash2 } from "lucide-react";
 import DeleteAccessTokenDialog from "@/components/accessTokens/DeleteAccessTokenDialog";
-import { AccessTokenWithFolder } from "@/lib/definitions";
+import { AccessTokenWithFolder, LightFolder } from "@/lib/definitions";
 import { useEffect, useState } from "react";
-import { personColumns } from "./columns-persons";
 import { useTranslations } from "next-intl";
+import { linksColumns } from "./columns";
 
-export default function PersonDataTable({
+export default function LinksDataTable({
 	accessTokens,
 	defaultTokenIndex,
+	lightFolders,
 }: {
 	readonly accessTokens: AccessTokenWithFolder[];
 	readonly defaultTokenIndex: number;
+	readonly lightFolders: LightFolder[];
 }) {
-	const t = useTranslations("dataTables.people.columns");
-	const [selectedTokensIndexes, setSelectedTokensIndexes] = useState<{ [index: number]: boolean }>({
-		[defaultTokenIndex]: true,
-	});
-	const [selectedTokens, setSelectedTokens] = useState<string[]>([accessTokens[defaultTokenIndex]?.token]);
+	const t = useTranslations("dataTables.links");
+	const [selectedTokensIndexes, setSelectedTokensIndexes] = useState<{ [index: number]: boolean }>(
+		defaultTokenIndex === -1 ? {} : { [defaultTokenIndex]: true }
+	);
+	const [selectedTokens, setSelectedTokens] = useState<string[]>(
+		defaultTokenIndex === -1 ? [] : [accessTokens[defaultTokenIndex].token]
+	);
+
 	const [lockOpen, setLockOpen] = useState<boolean>(false);
 	const [deleteOpen, setDeleteOpen] = useState<boolean>(false);
 	const [deleteSelectionOpen, setDeleteSelectionOpen] = useState<boolean>(false);
+
 	useEffect(() => {
-		setSelectedTokens(Object.keys(selectedTokensIndexes).map(k => accessTokens[Number.parseInt(k)]?.token));
+		setSelectedTokens(Object.keys(selectedTokensIndexes).map(k => accessTokens[Number.parseInt(k)].token));
 	}, [selectedTokensIndexes, accessTokens]);
 
 	return (
 		<DataTable
-			columns={personColumns}
+			columns={linksColumns.filter(c => c.id !== "email")}
 			data={accessTokens}
 			selection={selectedTokensIndexes}
 			setSelection={setSelectedTokensIndexes}
-			filterPlaceholder="Search links"
+			filterPlaceholder={t("actions.search.placeholder")}
 			filterColumn="folder_name"
 			rightHeadingNodes={
 				<div className="flex gap-2">
@@ -55,10 +62,18 @@ export default function PersonDataTable({
 										: "opacity-100"
 								}
 							>
-								<Trash2 className="w-4 h-4 mr-2" /> Delete selection
+								<Trash2 className="w-4 h-4 mr-2" />{" "}
+								{t("actions.deleteSelected")}
 							</Button>
 						</DialogTrigger>
 					</DeleteAccessTokenDialog>
+					<CreateAccessTokenDialog folders={lightFolders}>
+						<DialogTrigger asChild>
+							<Button variant="outline">
+								<Link className="w-4 h-4 mr-2" /> {t("actions.create")}
+							</Button>
+						</DialogTrigger>
+					</CreateAccessTokenDialog>
 				</div>
 			}
 			translations={t}
