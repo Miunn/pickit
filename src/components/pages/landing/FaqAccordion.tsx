a63d4232e410f8f3d17d0a2d145f37c54b1c6815
Rémi Caulier <remi@remicaulier.fr>
2026-03-08 10:05:48 +0100

    fix(upload): use same text with files over (#317)
    * fix(upload): use same text with files over
    
    * fix(landing): add key ids instead of using index

--- src/components/pages/landing/FaqAccordion.tsx
+++ src/components/pages/landing/FaqAccordion.tsx
@@ -2,75 +2,63 @@ import { useTranslations } from "next-intl";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Label } from "@/components/ui/label";
 
+const QUESTIONS = [
+	{
+		key: "question1",
+		title: "question1.title",
+		answer: "question1.answer",
+	},
+	{
+		key: "question2",
+		title: "question2.title",
+		answer: "question2.answer",
+	},
+	{
+		key: "question3",
+		title: "question3.title",
+		answer: "question3.answer",
+	},
+	{
+		key: "question4",
+		title: "question4.title",
+		answer: "question4.answer",
+	},
+	{
+		key: "question5",
+		title: "question5.title",
+		answer: "question5.answer",
+	},
+];
+
 export default function FaqAccordion() {
+	const t = useTranslations("components.faq");
 
-    const t = useTranslations("components.faq");
-
-    return (
-        <div className="max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-2xl xl:max-w-4xl mx-auto flex flex-col items-center my-32">
-            <Label className="text-primary font-semibold mb-3 tracking-wider text-base">{t('titleMini')}</Label>
-            <h3 className="text-xl sm:text-4xl md:text-5xl mb-6 md:mb-11">{t('title')}</h3>
-            <Accordion type="single" collapsible className="w-full space-y-2" defaultValue="2">
-                <AccordionItem
-                    value="1"
-                    className="bg-background has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
-                >
-                    <AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
-                        {t('question1.title')}
-                    </AccordionTrigger>
-                    <AccordionContent className="text-muted-foreground pb-2">
-                        <span dangerouslySetInnerHTML={{ __html: t('question1.answer') }} />
-                    </AccordionContent>
-                </AccordionItem>
-
-                <AccordionItem
-                    value="2"
-                    className="bg-background has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
-                >
-                    <AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
-                        {t('question2.title')}
-                    </AccordionTrigger>
-                    <AccordionContent className="text-muted-foreground pb-2">
-                        {t('question2.answer')}
-                    </AccordionContent>
-                </AccordionItem>
-
-                <AccordionItem
-                    value="3"
-                    className="bg-background has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
-                >
-                    <AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
-                        {t('question3.title')}
-                    </AccordionTrigger>
-                    <AccordionContent className="text-muted-foreground pb-2">
-                        {t('question3.answer')}
-                    </AccordionContent>
-                </AccordionItem>
-
-                <AccordionItem
-                    value="4"
-                    className="bg-background has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
-                >
-                    <AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
-                        {t('question4.title')}
-                    </AccordionTrigger>
-                    <AccordionContent className="text-muted-foreground pb-2">
-                        {t('question4.answer')}
-                    </AccordionContent>
-                </AccordionItem>
-
-                <AccordionItem
-                    value="5"
-                    className="bg-background has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
-                >
-                    <AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
-                        {t('question5.title')}
-                    </AccordionTrigger>
-                    <AccordionContent className="text-muted-foreground pb-2">
-                        {t('question5.answer')}
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
-        </div>
-    );
+	return (
+		<div className="max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-2xl xl:max-w-4xl mx-auto flex flex-col items-center my-32">
+			<Label className="text-primary font-semibold mb-3 tracking-wider text-base">
+				{t("titleMini")}
+			</Label>
+			<h3 className="text-xl sm:text-4xl md:text-5xl mb-6 md:mb-11">{t("title")}</h3>
+			<Accordion type="single" collapsible className="w-full space-y-2" defaultValue="2">
+				{QUESTIONS.map((question, index) => (
+					<AccordionItem
+						key={question.key}
+						value={(index + 1).toString()}
+						className="bg-background has-focus-visible:border-ring has-focus-visible:ring-ring/50 rounded-md border px-4 py-1 outline-none last:border-b has-focus-visible:ring-[3px]"
+					>
+						<AccordionTrigger className="py-2 text-[15px] leading-6 hover:no-underline focus-visible:ring-0">
+							{t(question.title)}
+						</AccordionTrigger>
+						<AccordionContent className="text-muted-foreground pb-2">
+							<span
+								dangerouslySetInnerHTML={{
+									__html: t(question.answer),
+								}}
+							/>
+						</AccordionContent>
+					</AccordionItem>
+				))}
+			</Accordion>
+		</div>
+	);
 }
