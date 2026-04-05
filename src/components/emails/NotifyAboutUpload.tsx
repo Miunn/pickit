275f36344a6510d09b23ed58f402ff44ace9a1fb
Rémi Caulier <remi@remicaulier.fr>
2026-04-05 10:39:56 +0200

    fix(logo): urls

--- src/components/emails/NotifyAboutUpload.tsx
+++ src/components/emails/NotifyAboutUpload.tsx
@@ -4,198 +4,236 @@ import EmailFooter from "@/components/emails/EmailFooter";
 const baseUrl = process.env.NEXT_PUBLIC_APP_URL ? process.env.NEXT_PUBLIC_APP_URL : "http://localhost:3000";
 
 export default function NotifyAboutUploadTemplate({
-    name,
-    folderName,
-    link,
-    isLocked,
-    count,
-    lang = "fr",
+	name,
+	folderName,
+	link,
+	isLocked,
+	count,
+	lang = "fr",
 }: {
-    readonly name: string;
-    readonly folderName: string;
-    readonly link: string;
-    readonly isLocked: boolean;
-    readonly count: number;
-    readonly lang: string;
+	readonly name: string;
+	readonly folderName: string;
+	readonly link: string;
+	readonly isLocked: boolean;
+	readonly count: number;
+	readonly lang: string;
 }) {
-    if (lang === "fr") {
-        return (
-            <Html>
-                <Head>
-                    <title>Nouveaux fichiers dans {folderName}</title>
+	if (lang === "fr") {
+		return (
+			<Html>
+				<Head>
+					<title>Nouveaux fichiers dans {folderName}</title>
 
-                    <Font
-                        fontFamily="Inter"
-                        fallbackFontFamily="Helvetica"
-                        webFont={{
-                            url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
-                            format: "woff2",
-                        }}
-                        fontWeight={400}
-                        fontStyle="normal"
-                    />
-                </Head>
+					<Font
+						fontFamily="Inter"
+						fallbackFontFamily="Helvetica"
+						webFont={{
+							url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
+							format: "woff2",
+						}}
+						fontWeight={400}
+						fontStyle="normal"
+					/>
+				</Head>
 
-                <Tailwind>
-                    <Body className="m-0 p-0 max-w-[600px] mx-auto mt-[30px]">
-                        <Section
-                            style={{
-                                border: "1px solid #e0e0e0",
-                                borderRadius: "5px",
-                            }}
-                        >
-                            <Row>
-                                <Column className="text-center bg-[#f4f4f4] p-4" align="center">
-                                    <Section width="100%">
-                                        <tr>
-                                            <td align="center" valign="middle">
-                                                <Img
-                                                    src={`${baseUrl}/static/logo-full-primary.png`}
-                                                    alt={process.env.NEXT_PUBLIC_APP_NAME}
-                                                    className="inline-block max-w-[200px] max-h-[32px]"
-                                                />
-                                            </td>
-                                        </tr>
-                                    </Section>
-                                </Column>
-                            </Row>
-                            <Row>
-                                <Column
-                                    style={{
-                                        padding: "20px",
-                                    }}
-                                >
-                                    <Text className="text-center text-xl tracking-wide">
-                                        Nouveaux fichiers ajoutés à <strong>{folderName}</strong>
-                                    </Text>
+				<Tailwind>
+					<Body className="m-0 p-0 max-w-[600px] mx-auto mt-[30px]">
+						<Section
+							style={{
+								border: "1px solid #e0e0e0",
+								borderRadius: "5px",
+							}}
+						>
+							<Row>
+								<Column
+									className="text-center bg-[#f4f4f4] p-4"
+									align="center"
+								>
+									<Section width="100%">
+										<tr>
+											<td
+												align="center"
+												valign="middle"
+											>
+												<Img
+													src={`${baseUrl}/static/logo.png`}
+													alt={
+														process
+															.env
+															.NEXT_PUBLIC_APP_NAME
+													}
+													className="inline-block max-w-[200px] max-h-[32px]"
+												/>
+											</td>
+										</tr>
+									</Section>
+								</Column>
+							</Row>
+							<Row>
+								<Column
+									style={{
+										padding: "20px",
+									}}
+								>
+									<Text className="text-center text-xl tracking-wide">
+										Nouveaux fichiers ajoutés à{" "}
+										<strong>{folderName}</strong>
+									</Text>
 
-                                    <Text>
-                                        {name} a ajouté{" "}
-                                        {count === 1 ? "1 nouveau fichier" : `${count} nouveaux fichiers`} à{" "}
-                                        <strong>{folderName}</strong>
-                                    </Text>
+									<Text>
+										{name} a ajouté{" "}
+										{count === 1
+											? "1 nouveau fichier"
+											: `${count} nouveaux fichiers`}{" "}
+										à <strong>{folderName}</strong>
+									</Text>
 
-                                    <Text>Cliquez sur le bouton ci-dessous pour y accéder :</Text>
+									<Text>
+										Cliquez sur le bouton ci-dessous pour y
+										accéder :
+									</Text>
 
-                                    <div className="bg-[#f9f9f9] p-[10px] my-[15px] border-dashed border-[1px] border-[#cccccc]">
-                                        <Button
-                                            href={link}
-                                            className="box-border w-full rounded-[8px] bg-[#1F7551] px-[12px] py-[12px] text-center font-semibold text-white"
-                                        >
-                                            {folderName}
-                                        </Button>
-                                    </div>
+									<div className="bg-[#f9f9f9] p-[10px] my-[15px] border-dashed border-[1px] border-[#cccccc]">
+										<Button
+											href={link}
+											className="box-border w-full rounded-[8px] bg-[#1F7551] px-[12px] py-[12px] text-center font-semibold text-white"
+										>
+											{folderName}
+										</Button>
+									</div>
 
-                                    {isLocked ? (
-                                        <Text>
-                                            {name} a vérouillé ce dossier par un code que vous devrez renseigner avant
-                                            de pouvoir accéder à son contenu. Si vous ne connaissez pas ce code,
-                                            veuillez contacter {name} pour l&apos;obtenir.
-                                        </Text>
-                                    ) : null}
+									{isLocked ? (
+										<Text>
+											{name} a vérouillé ce dossier
+											par un code que vous devrez
+											renseigner avant de pouvoir
+											accéder à son contenu. Si vous
+											ne connaissez pas ce code,
+											veuillez contacter {name} pour
+											l&apos;obtenir.
+										</Text>
+									) : null}
 
-                                    <Text>
-                                        Cordialement,
-                                        <br />
-                                        L&apos;équipe <strong>{process.env.NEXT_PUBLIC_APP_NAME}</strong>
-                                    </Text>
-                                </Column>
-                            </Row>
+									<Text>
+										Cordialement,
+										<br />
+										L&apos;équipe{" "}
+										<strong>
+											{
+												process.env
+													.NEXT_PUBLIC_APP_NAME
+											}
+										</strong>
+									</Text>
+								</Column>
+							</Row>
 
-                            <EmailFooter />
-                        </Section>
-                    </Body>
-                </Tailwind>
-            </Html>
-        );
-    }
+							<EmailFooter />
+						</Section>
+					</Body>
+				</Tailwind>
+			</Html>
+		);
+	}
 
-    return (
-        <Html>
-            <Head>
-                <title>New files in {folderName}</title>
+	return (
+		<Html>
+			<Head>
+				<title>New files in {folderName}</title>
 
-                <Font
-                    fontFamily="Inter"
-                    fallbackFontFamily="Helvetica"
-                    webFont={{
-                        url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
-                        format: "woff2",
-                    }}
-                    fontWeight={400}
-                    fontStyle="normal"
-                />
-            </Head>
+				<Font
+					fontFamily="Inter"
+					fallbackFontFamily="Helvetica"
+					webFont={{
+						url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
+						format: "woff2",
+					}}
+					fontWeight={400}
+					fontStyle="normal"
+				/>
+			</Head>
 
-            <Tailwind>
-                <Body className="m-0 p-0 max-w-[600px] mx-auto mt-[30px]">
-                    <Section
-                        style={{
-                            border: "1px solid #e0e0e0",
-                            borderRadius: "5px",
-                        }}
-                    >
-                        <Row>
-                            <Column className="text-center bg-[#f4f4f4] p-4" align="center">
-                                <Section width="100%">
-                                    <tr>
-                                        <td align="center" valign="middle">
-                                            <Img
-                                                src={`${baseUrl}/static/logo-full-primary.png`}
-                                                alt={process.env.NEXT_PUBLIC_APP_NAME}
-                                                className="inline-block max-w-[200px] max-h-[32px]"
-                                            />
-                                        </td>
-                                    </tr>
-                                </Section>
-                            </Column>
-                        </Row>
-                        <Row>
-                            <Column
-                                style={{
-                                    padding: "20px",
-                                }}
-                            >
-                                <Text className="text-center text-xl tracking-wide">
-                                    New files added to <strong>{folderName}</strong>
-                                </Text>
+			<Tailwind>
+				<Body className="m-0 p-0 max-w-[600px] mx-auto mt-[30px]">
+					<Section
+						style={{
+							border: "1px solid #e0e0e0",
+							borderRadius: "5px",
+						}}
+					>
+						<Row>
+							<Column className="text-center bg-[#f4f4f4] p-4" align="center">
+								<Section width="100%">
+									<tr>
+										<td align="center" valign="middle">
+											<Img
+												src={`${baseUrl}/static/logo.png`}
+												alt={
+													process.env
+														.NEXT_PUBLIC_APP_NAME
+												}
+												className="inline-block max-w-[200px] max-h-[32px]"
+											/>
+										</td>
+									</tr>
+								</Section>
+							</Column>
+						</Row>
+						<Row>
+							<Column
+								style={{
+									padding: "20px",
+								}}
+							>
+								<Text className="text-center text-xl tracking-wide">
+									New files added to <strong>{folderName}</strong>
+								</Text>
 
-                                <Text>
-                                    {name} added {count === 1 ? "1 new file" : `${count} new files`} added to{" "}
-                                    <strong>{folderName}</strong>
-                                </Text>
+								<Text>
+									{name} added{" "}
+									{count === 1
+										? "1 new file"
+										: `${count} new files`}{" "}
+									added to <strong>{folderName}</strong>
+								</Text>
 
-                                <Text>Click the button below to access it :</Text>
+								<Text>Click the button below to access it :</Text>
 
-                                <div className="bg-[#f9f9f9] p-[10px] my-[15px] border-dashed border-[1px] border-[#cccccc]">
-                                    <Button
-                                        href={link}
-                                        className="box-border w-full rounded-[8px] bg-[#1F7551] px-[12px] py-[12px] text-center font-semibold text-white"
-                                    >
-                                        {folderName}
-                                    </Button>
-                                </div>
+								<div className="bg-[#f9f9f9] p-[10px] my-[15px] border-dashed border-[1px] border-[#cccccc]">
+									<Button
+										href={link}
+										className="box-border w-full rounded-[8px] bg-[#1F7551] px-[12px] py-[12px] text-center font-semibold text-white"
+									>
+										{folderName}
+									</Button>
+								</div>
 
-                                {isLocked ? (
-                                    <Text>
-                                        {name} locked this folder with a code that you will need to enter before you can
-                                        access its content. If you don&apos;t know this code, please contact {name} to
-                                        get it.
-                                    </Text>
-                                ) : null}
+								{isLocked ? (
+									<Text>
+										{name} locked this folder with a code
+										that you will need to enter before you
+										can access its content. If you
+										don&apos;t know this code, please
+										contact {name} to get it.
+									</Text>
+								) : null}
 
-                                <Text>
-                                    Best regards,
-                                    <br />
-                                    The <strong>{process.env.NEXT_PUBLIC_APP_NAME}</strong> team
-                                </Text>
-                            </Column>
-                        </Row>
+								<Text>
+									Best regards,
+									<br />
+									The{" "}
+									<strong>
+										{process.env.NEXT_PUBLIC_APP_NAME}
+									</strong>{" "}
+									team
+								</Text>
+							</Column>
+						</Row>
 
-                        <EmailFooter />
-                    </Section>
-                </Body>
-            </Tailwind>
-        </Html>
-    );
+						<EmailFooter />
+					</Section>
+				</Body>
+			</Tailwind>
+		</Html>
+	);
 }
--- src/components/emails/ShareFolderTemplate.tsx
+++ src/components/emails/ShareFolderTemplate.tsx
@@ -4,116 +4,130 @@ import EmailFooter from "@/components/emails/EmailFooter";
 const baseUrl = process.env.NEXT_PUBLIC_APP_URL ? process.env.NEXT_PUBLIC_APP_URL : "http://localhost:3000";
 
 export default function ShareFolderTemplate({
-    name,
-    folderName,
-    link,
-    isLocked,
-    message,
+	name,
+	folderName,
+	link,
+	isLocked,
+	message,
 }: {
-    readonly name: string;
-    readonly folderName: string;
-    readonly link: string;
-    readonly isLocked: boolean;
-    readonly message?: string;
+	readonly name: string;
+	readonly folderName: string;
+	readonly link: string;
+	readonly isLocked: boolean;
+	readonly message?: string;
 }) {
-    return (
-        <Html>
-            <Head>
-                <title>Folder shared</title>
+	return (
+		<Html>
+			<Head>
+				<title>Folder shared</title>
 
-                <Font
-                    fontFamily="Inter"
-                    fallbackFontFamily="Helvetica"
-                    webFont={{
-                        url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
-                        format: "woff2",
-                    }}
-                    fontWeight={400}
-                    fontStyle="normal"
-                />
-            </Head>
+				<Font
+					fontFamily="Inter"
+					fallbackFontFamily="Helvetica"
+					webFont={{
+						url: "https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfAZ9hiA.woff2",
+						format: "woff2",
+					}}
+					fontWeight={400}
+					fontStyle="normal"
+				/>
+			</Head>
 
-            <Tailwind>
-                <Body className="m-0 p-0 max-w-[600px] mx-auto mt-[30px]">
-                    <Section
-                        style={{
-                            border: "1px solid #e0e0e0",
-                            borderRadius: "5px",
-                        }}
-                    >
-                        <Row>
-                            <Column className="text-center bg-[#f4f4f4] p-4" align="center">
-                                <Section width="100%">
-                                    <tr>
-                                        <td align="center" valign="middle">
-                                            <Img
-                                                src={`${baseUrl}/static/logo-full-primary.png`}
-                                                alt={process.env.NEXT_PUBLIC_APP_NAME}
-                                                className="inline-block max-w-[200px] max-h-[32px]"
-                                            />
-                                        </td>
-                                    </tr>
-                                </Section>
-                            </Column>
-                        </Row>
-                        <Row>
-                            <Column
-                                style={{
-                                    padding: "20px",
-                                }}
-                            >
-                                <Text className="text-center text-xl tracking-wide">
-                                    Un dossier vous a été partagé !
-                                </Text>
+			<Tailwind>
+				<Body className="m-0 p-0 max-w-[600px] mx-auto mt-[30px]">
+					<Section
+						style={{
+							border: "1px solid #e0e0e0",
+							borderRadius: "5px",
+						}}
+					>
+						<Row>
+							<Column className="text-center bg-[#f4f4f4] p-4" align="center">
+								<Section width="100%">
+									<tr>
+										<td align="center" valign="middle">
+											<Img
+												src={`${baseUrl}/static/logo.png`}
+												alt={
+													process.env
+														.NEXT_PUBLIC_APP_NAME
+												}
+												className="inline-block max-w-[200px] max-h-[32px]"
+											/>
+										</td>
+									</tr>
+								</Section>
+							</Column>
+						</Row>
+						<Row>
+							<Column
+								style={{
+									padding: "20px",
+								}}
+							>
+								<Text className="text-center text-xl tracking-wide">
+									Un dossier vous a été partagé !
+								</Text>
 
-                                <Text>Bonjour,</Text>
+								<Text>Bonjour,</Text>
 
-                                <Text>
-                                    <strong>{name}</strong> a partagé le dossier <strong>{folderName}</strong> avec vous
-                                    sur {process.env.NEXT_PUBLIC_APP_NAME}
-                                    {message ? ` et a ajouté le message suivant :` : "."}
-                                </Text>
+								<Text>
+									<strong>{name}</strong> a partagé le dossier{" "}
+									<strong>{folderName}</strong> avec vous sur{" "}
+									{process.env.NEXT_PUBLIC_APP_NAME}
+									{message
+										? ` et a ajouté le message suivant :`
+										: "."}
+								</Text>
 
-                                {message && (
-                                    <div className="bg-[#f9f9f9] p-[15px] my-[15px] border-[1px] border-[#cccccc] rounded-[5px]">
-                                        <Text className="whitespace-pre-wrap">{message}</Text>
-                                    </div>
-                                )}
+								{message && (
+									<div className="bg-[#f9f9f9] p-[15px] my-[15px] border-[1px] border-[#cccccc] rounded-[5px]">
+										<Text className="whitespace-pre-wrap">
+											{message}
+										</Text>
+									</div>
+								)}
 
-                                <Text>
-                                    Cliquez sur le bouton ci-dessous pour y accéder et découvrir les photos et vidéos
-                                    qu&apos;il contient :
-                                </Text>
+								<Text>
+									Cliquez sur le bouton ci-dessous pour y accéder
+									et découvrir les photos et vidéos qu&apos;il
+									contient :
+								</Text>
 
-                                <div className="bg-[#f9f9f9] p-[10px] my-[15px] border-dashed border-[1px] border-[#cccccc]">
-                                    <Button
-                                        href={link}
-                                        className="box-border w-full rounded-[8px] bg-[#1F7551] px-[12px] py-[12px] text-center font-semibold text-white"
-                                    >
-                                        {folderName}
-                                    </Button>
-                                </div>
+								<div className="bg-[#f9f9f9] p-[10px] my-[15px] border-dashed border-[1px] border-[#cccccc]">
+									<Button
+										href={link}
+										className="box-border w-full rounded-[8px] bg-[#1F7551] px-[12px] py-[12px] text-center font-semibold text-white"
+									>
+										{folderName}
+									</Button>
+								</div>
 
-                                {isLocked ? (
-                                    <Text>
-                                        {name} a vérouillé ce dossier par un code que vous devrez renseigner avant de
-                                        pouvoir accéder à son contenu. Si vous ne connaissez pas ce code, veuillez
-                                        contacter {name} pour l&apos;obtenir.
-                                    </Text>
-                                ) : null}
+								{isLocked ? (
+									<Text>
+										{name} a vérouillé ce dossier par un
+										code que vous devrez renseigner avant de
+										pouvoir accéder à son contenu. Si vous
+										ne connaissez pas ce code, veuillez
+										contacter {name} pour l&apos;obtenir.
+									</Text>
+								) : null}
 
-                                <Text>
-                                    Cordialement,
-                                    <br />
-                                    L&apos;équipe <strong>{process.env.NEXT_PUBLIC_APP_NAME}</strong>
-                                </Text>
-                            </Column>
-                        </Row>
+								<Text>
+									Cordialement,
+									<br />
+									L&apos;équipe{" "}
+									<strong>
+										{process.env.NEXT_PUBLIC_APP_NAME}
+									</strong>
+								</Text>
+							</Column>
+						</Row>
 
-                        <EmailFooter />
-                    </Section>
-                </Body>
-            </Tailwind>
-        </Html>
-    );
+						<EmailFooter />
+					</Section>
+				</Body>
+			</Tailwind>
+		</Html>
+	);
 }
